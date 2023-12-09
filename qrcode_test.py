b3e77e9acaae9b161a40777ac1a194d19c33be7c
Sávio Coelho <116724820+Sav-Coelho@users.noreply.github.com>
2023-12-09 18:14:17 -0300

    Add files via upload

---
+++ qrcode_test.py
@@ -0,0 +1,22 @@
+### No presente código irei gerar uma solução em python visando automatizar criação de códigos QR###
+
+import qrcode
+
+
+def gerar_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction= qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+
+    )
+
+    qr.add_data(text)
+    qr.make(fit = True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg001.png")
+
+
+url = input("Digite sua URL: ")
+gerar_qrcode(url)
