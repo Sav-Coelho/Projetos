c266a5ee3e46bba7f92994cf75d8bdb0aa2a9ad6
Sávio Coelho <116724820+Sav-Coelho@users.noreply.github.com>
2024-01-02 20:26:59 -0300

    Update_qrcode_test.py

--- qrcode_test.py
+++ qrcode_test.py
@@ -1,6 +1,7 @@
 ### No presente código irei gerar uma solução em python visando automatizar criação de códigos QR###
 
 import qrcode
+import random
 
 
 def gerar_qrcode(text):
@@ -15,7 +16,12 @@ def gerar_qrcode(text):
     qr.add_data(text)
     qr.make(fit = True)
     img = qr.make_image(fill_color="yellow", back_color="black")
-    img.save("qrimg001.png")
+    lista1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ,'l' ,'m', 'n', 'q', 'o', 'p', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    lista2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    caract1= random.choice(lista1)
+    caract2= random.choice(lista2)
+    caract3= ".png"
+    img.save(caract1 + caract2 + caract3)
 
 
 url = input("Digite sua URL: ")
